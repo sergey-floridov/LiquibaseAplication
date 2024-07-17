adfa500f68637bed0b2c0ac222c0d9d24c507810
Флоридов Сергей Сергеевич (Sergey Floridov) <Sfloridov@egartech.ru>
2024-07-17 23:23:50 +0300

    init

---
+++ src/main/java/com/example/liquibaseaplication/LiquibaseAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseAplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseAplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liquibaseaplication/LiquibaseAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseAplicationApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
